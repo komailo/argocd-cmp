2661ce2a7159bdc4da809dfd34e6fefebc8e1d7d
Komail Kanjee <1796298+KomailKanjee@users.noreply.github.com>
2025-01-01 03:23:16 -0500

    cache github token to file system

--- root/cmp-sidecar-scripts/get-values-files.py
+++ root/cmp-sidecar-scripts/get-values-files.py
@@ -9,81 +9,108 @@ from datetime import datetime
 import jwt
 import requests
 
-GITHUB_TOKEN = ""
-GITHUB_TOKEN_EXPIRY = ""
 
+class GitHubFile(object):
 
-def generate_jwt(app_id, private_key):
-    now = int(time.time())
-    payload = {
-        "iat": now,  # Issued at time
-        "exp": now + 600,  # Token expiration time (10 minutes)
-        "iss": app_id,  # GitHub App ID
-    }
-    headers = {"alg": "RS256", "typ": "JWT"}
-    return jwt.encode(payload, private_key, algorithm="RS256", headers=headers)
-
-
-def get_installation_token(jwt_token, installation_id):
-    url = f"https://api.github.com/app/installations/{installation_id}/access_tokens"
-    headers = {
-        "Authorization": f"Bearer {jwt_token}",
-        "Accept": "application/vnd.github+json",
-    }
-    response = requests.post(url, headers=headers)
-    if response.status_code == 201:
-        token = response.json()["token"]
-        expiry = response.json()["expires_at"]
-        return token, expiry
-    else:
-        raise Exception(f"Error: {response.status_code}, {response.json()}")
-
-
-def github_token():
-    global GITHUB_TOKEN
-    global GITHUB_TOKEN_EXPIRY
-
-    if GITHUB_TOKEN and GITHUB_TOKEN_EXPIRY:
-        time_to_compare = GITHUB_TOKEN_EXPIRY
-        time_to_compare_datetime = datetime.strptime(
-            time_to_compare, "%Y-%m-%dT%H:%M:%SZ"
+    def __init__(self):
+        self.github_token = None
+        self.github_token_expiry = None
+        self.work_dir = os.environ.get(
+            "CMP_SIDECAR_WORKDIR", "/tmp/cmp-sidecar-workdir"
         )
-        if datetime.now() < time_to_compare_datetime:
-            return GITHUB_TOKEN
+        self.load_github_token()
 
-    app_id = os.environ["GITHUB_APP_ID"]
-    private_key = os.environ["GITHUB_PRIVATE_KEY"]
-    installation_id = os.environ["GITHUB_APP_INSTALLATION_ID"]
-    jwt_token = generate_jwt(app_id, private_key)
+    def load_github_token(self):
+        if not os.path.exists(self.work_dir):
+            return
+        elif not os.path.exists(os.path.join(self.work_dir, "github_token")):
+            return
+        elif not os.path.exists(os.path.join(self.work_dir, "github_token_expiry")):
+            return
 
-    installation_id = os.environ.get("GITHUB_APP_INSTALLATION_ID")
-    GITHUB_TOKEN, GITHUB_TOKEN_EXPIRY = get_installation_token(
-        jwt_token, installation_id
-    )
+        with open(os.path.join(self.work_dir, "github_token"), "r") as f:
+            self.github_token = f.read().strip()
 
-    return GITHUB_TOKEN
+        with open(os.path.join(self.work_dir, "github_token_expiry"), "r") as f:
+            self.github_token_expiry = f.read().strip()
 
+    def save_github_token(self):
+        os.mkdir(self.work_dir, exist_ok=True)
+        with open(os.path.join(self.work_dir, "github_token"), "w") as f:
+            f.write(self.github_token)
 
-def github_get_file_contents(org, repository, file_path, branch=None):
-    url = f"https://api.github.com/repos/{org}/{repository}/contents/{file_path}"
-    if branch:
-        url += f"?ref={branch}"
+        with open(os.path.join(self.work_dir, "github_token_expiry"), "w") as f:
+            f.write(self.github_token_expiry)
 
-    headers = {
-        "Authorization": f"token {github_token()}",
-    }
+    def generate_jwt(self, app_id, private_key):
+        now = int(time.time())
+        payload = {
+            "iat": now,  # Issued at time
+            "exp": now + 600,  # Token expiration time (10 minutes)
+            "iss": app_id,  # GitHub App ID
+        }
+        headers = {"alg": "RS256", "typ": "JWT"}
+        return jwt.encode(payload, private_key, algorithm="RS256", headers=headers)
 
-    response = requests.get(url, headers=headers)
+    def get_installation_token(self, jwt_token, installation_id):
+        url = (
+            f"https://api.github.com/app/installations/{installation_id}/access_tokens"
+        )
+        headers = {
+            "Authorization": f"Bearer {jwt_token}",
+            "Accept": "application/vnd.github+json",
+        }
+        response = requests.post(url, headers=headers)
+        if response.status_code == 201:
+            token = response.json()["token"]
+            expiry = response.json()["expires_at"]
+            return token, expiry
+        else:
+            raise Exception(f"Error: {response.status_code}, {response.json()}")
 
-    if response.status_code == 200:
-        file_content = response.json()
-        # Decode the base64 content
-        decoded_content = base64.b64decode(file_content["content"]).decode("utf-8")
-        return decoded_content
-    else:
-        print(f"Failed to fetch file: {response.status_code}")
-        print(response.json())
-        raise Exception(f"Failed to fetch file via url {url}: {response.status_code}")
+    def get_github_token(self):
+        if self.github_token and self.github_token_expiry:
+            time_to_compare = self.github_token_expiry
+            time_to_compare_datetime = datetime.strptime(
+                time_to_compare, "%Y-%m-%dT%H:%M:%SZ"
+            )
+            if datetime.now() < time_to_compare_datetime:
+                return self.github_token
+
+        app_id = os.environ["GITHUB_APP_ID"]
+        private_key = os.environ["GITHUB_PRIVATE_KEY"]
+        installation_id = os.environ["GITHUB_APP_INSTALLATION_ID"]
+        jwt_token = self.generate_jwt(app_id, private_key)
+
+        installation_id = os.environ.get("GITHUB_APP_INSTALLATION_ID")
+        self.github_token, self.github_token_expiry = self.get_installation_token(
+            jwt_token, installation_id
+        )
+        self.save_github_token()
+        return self.github_token
+
+    def github_get_file_contents(self, org, repository, file_path, branch=None):
+        url = f"https://api.github.com/repos/{org}/{repository}/contents/{file_path}"
+        if branch:
+            url += f"?ref={branch}"
+
+        headers = {
+            "Authorization": f"token {self.get_github_token()}",
+        }
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            file_content = response.json()
+            # Decode the base64 content
+            decoded_content = base64.b64decode(file_content["content"]).decode("utf-8")
+            return decoded_content
+        else:
+            print(f"Failed to fetch file: {response.status_code}")
+            print(response.json())
+            raise Exception(
+                f"Failed to fetch file via url {url}: {response.status_code}"
+            )
 
 
 class FileURIParamter(argparse.Action):
@@ -145,10 +172,12 @@ def main():
 
     args = parser.parse_args()
 
+    github_file = GitHubFile()
+
     for file_uri in args.file_uri:
         if file_uri["source"] == "github":
             print(file_uri, file=sys.stderr)
-            content = github_get_file_contents(
+            content = github_file.github_get_file_contents(
                 file_uri["org"],
                 file_uri["repository"],
                 file_uri["file_path"],
