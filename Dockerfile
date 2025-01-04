e5cd889347bdb428f9a12e14a7f01de85bb7589c
Komail Kanjee <1796298+komailo@users.noreply.github.com>
2025-01-04 15:15:26 -0500

    Restructure Dockerfile, generate and use IP in get-values-files.py (#9)
    * DNS resolution was failing natively so using dns resolver. Fixes https://github.com/komailo/argocd-cmp/issues/10
    * Use Bash arrays to compile the helm template args
    * Add helm and avp via Docker stage builds
    
    Co-authored-by: Komail Kanjee <1796298+KomailKanjee@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,23 @@
-FROM alpine:3
+FROM alpine:3 AS build
 
 ARG AVP_VERSION=1.16.1
 ARG HELM_VERSION=3.16.2
 
-# wget https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz -O - | tar xz && mv linux-amd64/helm /custom-tools/helm && chmod +x /custom-tools/helm
-# wget https://github.com/stedolan/jq/releases/download/jq-${JQ_VERSION}/jq-linux64 -O /custom-tools/jq && chmod +x /custom-tools/jq
-# wget https://github.com/mikefarah/yq/releases/download/v{YQ_VERSION}/yq_linux_amd64 -O /custom-tools/yq && chmod +x /custom-tools/yq
+RUN apk add --update --no-cache \
+    tar
 
-# # wget https://github.com/argoproj-labs/argocd-vault-plugin/releases/download/v${AVP_VERSION}/argocd-vault-plugin_${AVP_VERSION}_linux_amd64 -O /custom-tools/argocd-vault-plugin && chmod +x /custom-tools/argocd-vault-plugin
-# /custom-tools/argocd-vault-plugin version
+ADD  https://github.com/argoproj-labs/argocd-vault-plugin/releases/download/v${AVP_VERSION}/argocd-vault-plugin_${AVP_VERSION}_linux_amd64 \
+    /argocd-vault-plugin
+
+ADD https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz \
+    /helm-linux-amd64.tar.gz
+
+RUN mkdir /helm \
+    && cd /helm \
+    && tar -xvf /helm-linux-amd64.tar.gz \
+    && chmod +x /helm/linux-amd64/helm /argocd-vault-plugin
+
+FROM alpine:3
 
 RUN apk add --update --no-cache \
     bash \
@@ -18,6 +27,7 @@ RUN apk add --update --no-cache \
     openssl \
     py-pip  \
     py3-cryptography \
+    py3-dnspython \
     py3-jwt \
     py3-requests \
     python3 \
@@ -25,13 +35,8 @@ RUN apk add --update --no-cache \
 
 #RUN wget https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz -O - | tar xz && mv linux-amd64/helm /usr/local/bin/helm && chmod +x /custom-tools/helm /usr/local/bin
 
-ADD  --chmod=755 --chown=root:root \
-    https://github.com/argoproj-labs/argocd-vault-plugin/releases/download/v${AVP_VERSION}/argocd-vault-plugin_${AVP_VERSION}_linux_amd64 \
-    /usr/local/bin/argocd-vault-plugin
-
-ADD --chmod=755 --chown=root:root \
-    https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz \
-    /usr/local/bin/helm
+COPY --from=build /argocd-vault-plugin /usr/local/bin/argocd-vault-plugin
+COPY --from=build /helm/linux-amd64/helm /usr/local/bin/helm
 
 COPY --chmod=755 --chown=root:root \
     root/ \
--- root/cmp-sidecar-scripts/generate.sh
+++ root/cmp-sidecar-scripts/generate.sh
@@ -5,18 +5,30 @@ set -eu -o pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # we xargs the output to make them space separated vs new line
-ARGUMENTS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "values-files").array | .[] | "--values=" + .' | xargs)
+VALUE_FILES=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "values-files").array | .[] | "--values=" + .' | xargs)
 PARAMETERS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "helm-parameters").map | to_entries | map("\(.key)=\(.value)") | .[] | "--set=" + .' | xargs) 
 CLUSTER_NAME=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "cluster-name").string')
 HELM_RELEASE_NAME=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "helm-release-name").string')
 EXTERNAL_VALUES_FILES=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "external-values-files").array | .[]' | xargs)
 
-echo "Helm Arguments: '${ARGUMENTS}'" >&2
+echo "Helm Value Files: '${VALUE_FILES}'" >&2
 echo "Helm Parameters: '${PARAMETERS}'" >&2
 echo "Cluster Name: '${CLUSTER_NAME}'" >&2
 echo "External Values Files: '${EXTERNAL_VALUES_FILES}'" >&2
 
 mkdir -p "$EXTERNAL_FILES_DIR"
+
+HELM_TEMPLATE_CMD_ARGS=()
+
+HELM_TEMPLATE_CMD_ARGS+=("$HELM_RELEASE_NAME" . --namespace "$ARGOCD_APP_NAMESPACE")
+
+if [ -n "$VALUE_FILES" ]; then
+    IFS=' ' read -r -a HELM_TEMPLATE_CMD_ARGS <<< "$VALUE_FILES"
+fi
+
+if [ -n "$PARAMETERS" ]; then
+    IFS=' ' read -r -a HELM_TEMPLATE_CMD_ARGS <<< "$PARAMETERS"
+fi
 
 if [ -n "$EXTERNAL_VALUES_FILES" ]; then
     "$SCRIPT_DIR/get-values-files.py" --output-dir "$EXTERNAL_FILES_DIR" "$EXTERNAL_VALUES_FILES"
@@ -25,7 +37,7 @@ fi
 # append the external values files to the arguments. The external values files have to be converted to base64
 for external_values_file in $EXTERNAL_VALUES_FILES; do
     base64_external_values_file=$(echo -n "$external_values_file" | base64 -w 0)
-    ARGUMENTS="$ARGUMENTS --values $EXTERNAL_FILES_DIR/${base64_external_values_file}.yaml"
+    HELM_TEMPLATE_CMD_ARGS+=(--values "$EXTERNAL_FILES_DIR/${base64_external_values_file}.yaml")
 done
 
 if [ -z "$CLUSTER_NAME" ]; then
@@ -35,20 +47,10 @@ else
     export AVP_PATH_VALIDATION="^argocd:k8s-(${CLUSTER_NAME}|global)\..+"
 fi
 
-HELM_TEMPLATE_CMD="helm template --debug \"$HELM_RELEASE_NAME\" . --namespace \"$ARGOCD_APP_NAMESPACE\""
-
-if [ -n "$ARGUMENTS" ]; then
-    HELM_TEMPLATE_CMD="$HELM_TEMPLATE_CMD $ARGUMENTS"
-fi
-
-if [ -n "$PARAMETERS" ]; then
-    HELM_TEMPLATE_CMD="$HELM_TEMPLATE_CMD $PARAMETERS"
-fi
-
-echo "Helm Template Command: '${HELM_TEMPLATE_CMD}'" >&2
+echo "Helm Template Command Args: '${HELM_TEMPLATE_CMD_ARGS[*]}'" >&2
 
 # Run helm template command and capture output
-TEMPLATE_OUTPUT=$(eval "$HELM_TEMPLATE_CMD")
+TEMPLATE_OUTPUT=$(helm template "${HELM_TEMPLATE_CMD_ARGS[@]}")
 
 # Check if the template output is non-empty
 if [ -z "$TEMPLATE_OUTPUT" ]; then
